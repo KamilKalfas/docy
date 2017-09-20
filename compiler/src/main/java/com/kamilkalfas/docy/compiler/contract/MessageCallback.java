f91efd773b0929a654ec0b4d7c191535123b9334
Kamil Kalfas <kamil.kalfas@sky.uk>
2017-09-20 20:00:35 +0100

    wip

---
+++ compiler/src/main/java/com/kamilkalfas/docy/compiler/contract/MessageCallback.java
@@ -0,0 +1,4 @@
+package com.kamilkalfas.docy.compiler.contract;
+
+public interface MessageCallback {
+}
---
+++ compiler/src/main/java/com/kamilkalfas/docy/compiler/markdown/MarkdownPublisher.java
@@ -0,0 +1,4 @@
+package com.kamilkalfas.docy.compiler.markdown;
+
+public class MarkdownPublisher {
+}
