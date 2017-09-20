369640d2cac4866ee1081927b22079d5a687c8bc
Kamil Kalfas <kamil.kalfas@sky.uk>
2017-09-20 18:22:33 +0100

    env helper test

--- compiler/src/main/java/com/kamilkalfas/docy/compiler/env/EnvHelper.java
+++ compiler/src/main/java/com/kamilkalfas/docy/compiler/env/EnvHelper.java
@@ -1,12 +1,15 @@
 package com.kamilkalfas.docy.compiler.env;
 
 public class EnvHelper {
-    private static final String MODULES = "DOCY_MODULES";
+    public static final String MODULES = "DOCY_MODULES";
 
-    private EnvHelper() {}
+    public EnvHelper() {}
 
-    public static int getNumberOfModules() {
-        final String val = System.getenv(MODULES);
+    public int getNumberOfModules(final String var) {
+        if (null == var || var.isEmpty()) {
+            throw new IllegalArgumentException("Variable name null or empty!");
+        }
+        final String val = System.getenv(var);
         if (null == val || val.isEmpty()) {
             throw new IllegalStateException("Variable not defined");
         }
